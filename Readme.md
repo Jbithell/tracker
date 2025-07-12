1c1f1e4582bec78b3fce6dbe3d7881e999a9fdd4
James Bithell <Jbithell@users.noreply.github.com>
2025-07-12 19:16:51 +0100

    Add traccar endpoint

---
+++ Readme.md
@@ -0,0 +1,10 @@
+### Setup Traccar forwarding
+
+<entry key='forward.type'>url</entry>
+<entry key='forward.url'>https://traccar-forward-event-tracker.jbithell.com/upload-traccar.json</entry>
+<entry key='event.forward.url'>https://traccar-forward-event-tracker.jbithell.com/upload-traccar.json</entry>
+<entry key='forward.retry.enable'>true</entry>
+<entry key='forward.retry.delay'>10000</entry>
+<entry key='forward.retry.count'>1000</entry>
+<entry key='forward.retry.limit'>1000</entry>
+<entry key='event.forward.type'>json</entry>
---
+++ website/app/routes/upload-traccar[.json].ts
@@ -0,0 +1,43 @@
+import {
+  ActionFunctionArgs,
+  json,
+  redirect,
+  type LoaderFunctionArgs,
+} from "@remix-run/cloudflare";
+import { db } from "../d1client.server";
+import { withZod } from "@remix-validated-form/with-zod";
+import { number, z as zod } from "zod";
+import { GenericObject, validationError } from "remix-validated-form";
+import { and, eq, isNotNull, isNull } from "drizzle-orm";
+import { Events } from "~/db/schema/Events";
+
+export const loader = async ({ context, request }: ActionFunctionArgs) => {
+  const { env, cf } = context.cloudflare;
+  // Get parameters from the request
+  const url = new URL(request.url);
+  console.log("Request info", {
+    url,
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+  });
+  url.searchParams.forEach((value, key) => {
+    console.log(key, value);
+  });
+  return json({ message: "Not yet developed" }, 200);
+};
+
+export const action = async ({ context, request }: ActionFunctionArgs) => {
+  const { env, cf } = context.cloudflare;
+  if (request.method !== "PUT") {
+    return json({ message: "Method not allowed" }, 405);
+  }
+  let payload: unknown;
+  try {
+    payload = await request.json();
+  } catch (e) {
+    return json({ message: "Invalid JSON" }, 400);
+  }
+  console.log("Payload", payload);
+  return json({ message: "Not yet developed" }, 200);
+};
